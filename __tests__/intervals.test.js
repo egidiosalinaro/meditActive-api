b4bea4aa3fe693edba17b0259114385a3037bb6f
egy351 <129901135+egy351@users.noreply.github.com>
2023-09-26 13:29:24 +0200

    testing GET intervals query results

--- __tests__/intervals.test.js
+++ __tests__/intervals.test.js
@@ -1,3 +1,5 @@
+const { default: mongoose } = require('mongoose');
+
 const supertest = require('supertest');
 const app = require('../app');
 const testRequest = supertest(app);
@@ -13,13 +15,13 @@ const testInterval = new IntervalModel({
   relatedGoal: '6509d8f44159aaa41b0fc1f1',
 });
 
-let testId = null;
+let testId = new mongoose.Types.ObjectId().toString();
 
 describe('Intervals-related Endpoints', () => {
   beforeAll(async () => {
     await connectToDB();
     const testResponse = await testInterval.save();
-    testId = testResponse._id.toHexString();
+    testId = testResponse._id;
   });
   afterAll(async () => {
     await disconnectToDB();
@@ -40,6 +42,40 @@ describe('Intervals-related Endpoints', () => {
           relatedGoal: expect.any(String),
         }),
       ]);
+    });
+  });
+
+  describe('get a list of intervals filtered by query', () => {
+    describe('given the research returned no result', () => {
+      it('should return a 422 unprocessable entity status', async () => {
+        const dummyQuery = '631c8c9oDUMMYo7ccb8258b9';
+        await testRequest
+          .get(`/meditactive/intervals/filter?user=${dummyQuery}`)
+          .expect(422);
+      });
+    });
+
+    describe('given the filtered intervals exists', () => {
+      it('should return a 200 status and the filtered intervals', async () => {
+        const { body, statusCode } = await testRequest.get(
+          `/meditactive/intervals/filter?${
+            `user=${testInterval.user}` ||
+            `start=${testInterval.starting}` ||
+            `end=${testInterval.ending}` ||
+            `goal=${testInterval.relatedGoal}`
+          }`
+        );
+
+        expect(statusCode).toBe(200);
+        expect(body[0].user).toEqual(encodeURI(testInterval.user));
+        expect(JSON.stringify(body[0].starting)).toEqual(
+          JSON.stringify(testInterval.starting)
+        );
+        expect(JSON.stringify(body[0].ending)).toEqual(
+          JSON.stringify(testInterval.ending)
+        );
+        expect(body[0].relatedGoal).toBe(encodeURI(testInterval.relatedGoal));
+      });
     });
   });
 });
